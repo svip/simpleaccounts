f020d310f2d9e360124e8e63c7a1205751504db9
Svip <svip@theinfosphere.org>
2019-01-09 17:11:50 +0100

    Created database package with test.

---
+++ src/db/db.go
@@ -0,0 +1,194 @@
+package db
+
+import (
+	"encoding/json"
+	"time"
+	"fmt"
+	"sync"
+	"sort"
+	"os"
+	"log"
+	"io"
+)
+
+// TYPES
+
+// Money is generally accepted to have two decimal numbers, but storing floats
+// can have some unforseen consequences (like 0.1+0.2!=0.3).
+type Money int
+
+func NewMoney(a float64) Money {
+	// We'll just any further precision.  So yes, 1.505 becomes 1.50.
+	return Money(int(a*100))
+}
+
+func (m Money) ToFloat() float64 {
+	return float64(m)/100
+}
+
+func (m Money) String() string {
+	return fmt.Sprintf("%0.2f", m.ToFloat())
+} 
+
+type Transaction struct {
+	Time time.Time
+	Amount Money
+}
+
+type Account struct {
+	Id int
+	Name string
+	Transactions []Transaction
+}
+
+func (a Account) Sum() Money {
+	var sum Money
+	for _, trans := range a.Transactions {
+		sum += trans.Amount
+	}
+	return sum
+}
+
+func (a Account) String() string {
+	return fmt.Sprintf("%d: %s (%s)", a.Id, a.Name, a.Sum().String())
+}
+
+// GENERAL VARIABLES AND HELPER FUNCTIONS
+
+// Our database is merely a list of accounts.
+// Stored in a map for easy lookup.
+var database map[int]Account
+var mutex = &sync.Mutex{} // Our general database mutex.
+var savetofile bool
+const FILENAME = "database.json"
+
+func save() error {
+	if !savetofile {
+		return nil
+	}
+	file, err := os.Create(FILENAME)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	enc := json.NewEncoder(file)
+	if err := enc.Encode(&database); err != nil {
+		return err
+	}
+	return nil
+}
+
+// EXPORTED FUNCTIONS
+
+func Init(nofile bool) {
+	savetofile = !nofile
+	if savetofile {
+		file, err := os.Create(FILENAME)
+		if err != nil {
+			// If we cannot even open the file, then we might as well not run at all
+			log.Fatal(err)
+		}
+		defer file.Close()
+		dec := json.NewDecoder(file)
+		mutex.Lock()
+		if err = dec.Decode(&database); err != nil {
+			if err == io.EOF {
+				database = make(map[int]Account)
+			} else {
+				log.Fatal(err)
+			}
+		}
+		mutex.Unlock()
+	} else {
+		database = make(map[int]Account)
+	}
+}
+
+func EmptyDatabase() {
+	mutex.Lock()
+	database = make(map[int]Account)
+	mutex.Unlock()
+}
+
+func CreateAccount(name string) Account {
+	// We are keeping it simple, with just incrementing IDs.
+	mutex.Lock()
+	newid := len(database)+1
+	
+	account := Account{newid, name, []Transaction{}}
+	
+	database[newid] = account
+	
+	save()
+	
+	mutex.Unlock()
+	
+	return account
+}
+
+type byId []Account
+
+func (a byId) Len() int           { return len(a) }
+func (a byId) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byId) Less(i, j int) bool { return a[i].Id < a[j].Id }
+
+func GetAccounts() []Account {
+	var list []Account
+	mutex.Lock()
+	for _, acc := range database {
+		list = append(list, acc)
+	}
+	mutex.Unlock()
+	sort.Sort(byId(list))
+	return list
+}
+
+func GetAccount(id int) (Account, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	if acc, ok := database[id]; ok {
+		return acc, nil
+	} else {
+		return Account{}, fmt.Errorf("Account id %d not found.", id)
+	}
+}
+
+func CreateTransaction(accountid int, amount Money) error {
+	acc, err := GetAccount(accountid)
+	if err != nil {
+		return err
+	}
+	
+	acc.Transactions = append(acc.Transactions, Transaction{time.Now(), amount})
+	
+	mutex.Lock()
+	database[accountid] = acc
+	save()
+	mutex.Unlock()
+	
+	return nil
+}
+
+func DeleteTransaction(accountid int, transid time.Time) error {
+	acc, err := GetAccount(accountid)
+	if err != nil {
+		return err
+	}
+	
+	id := -1
+	for i, trans := range acc.Transactions {
+		if trans.Time == transid {
+			id = i
+		}
+	}
+	
+	acc.Transactions = append(acc.Transactions[:id], acc.Transactions[id+1:]...)
+	
+	mutex.Lock()
+	database[accountid] = acc
+	save()
+	mutex.Unlock()
+	
+	return nil
+}
+
---
+++ src/db/db_test.go
@@ -0,0 +1,103 @@
+package db
+
+import (
+	"testing"
+)
+
+// Right now, the tests are very basic.  But should we run into further errors,
+// we can now easier those tests to these.
+
+func TestCreateAccount(t *testing.T) {
+	Init(true)
+	testacc := Account{1, "TestName", []Transaction{}}
+	acc1 := CreateAccount(testacc.Name)
+	if acc1.Id != testacc.Id || acc1.Name != testacc.Name {
+		t.Error("Expected an account with Id 1 and name 'TestName', got ", acc1)
+	}
+	acc2, err := GetAccount(1)
+	if err != nil {
+		t.Error("Expected no error, got ", err)
+	}
+	if acc2.Id != testacc.Id || acc2.Name != testacc.Name {
+		t.Error("Expected an account with Id 1 and name 'TestName', got ", acc2)
+	}
+}
+
+func TestGetAccounts(t *testing.T) {
+	Init(true)
+	testacc := Account{1, "TestName", []Transaction{}}
+	CreateAccount(testacc.Name)
+	list := GetAccounts()
+	if len(list) != 1 {
+		t.Error("Expected a list of 1 accounts, got ", len(list))
+	}
+	acc1 := list[0]
+	if acc1.Id != testacc.Id || acc1.Name != testacc.Name {
+		t.Error("Expected an account with Id 1 and name 'TestName', got ", acc1)
+	}
+}
+
+func TestGetAccount(t *testing.T) {
+	Init(true)
+	testacc := Account{1, "TestName", []Transaction{}}
+	acc1 := CreateAccount(testacc.Name)
+	if acc1.Id != testacc.Id || acc1.Name != testacc.Name {
+		t.Error("Expected an account with Id 1 and name 'TestName', got ", acc1)
+	}
+	acc2, err := GetAccount(1)
+	if err != nil {
+		t.Error("Expected no error, got ", err)
+	}
+	if acc2.Id != testacc.Id || acc2.Name != testacc.Name {
+		t.Error("Expected an account with Id 1 and name 'TestName', got ", acc2)
+	}
+}
+
+func TestCreateTransaction(t *testing.T) {
+	Init(true)
+	testacc := Account{1, "TestName", []Transaction{}}
+	CreateAccount(testacc.Name)
+	err := CreateTransaction(1, NewMoney(10.00))
+	if err != nil {
+		t.Error("Expected no error, got ", err)
+	}
+	acc1, _ := GetAccount(1)
+	if len(acc1.Transactions) != 1 {
+		t.Error("Expected just one transaction, got ", len(acc1.Transactions))
+	}
+	if acc1.Transactions[0].Amount != NewMoney(10.00) {
+		t.Error("Expected an amount of 10.00, got ", acc1.Transactions[0].Amount)
+	}
+	if acc1.Sum() != NewMoney(10.00) {
+		t.Error("Expected a sum of 10.00, got ", acc1.Sum())
+	}
+	
+	CreateTransaction(1, NewMoney(20.00))
+	acc2, _ := GetAccount(1)
+	if acc2.Transactions[1].Amount != NewMoney(20.00) {
+		t.Error("Expected an amount of 20.00, got ", acc2.Transactions[1].Amount)
+	}
+	if acc2.Sum() != NewMoney(30.00) {
+		t.Error("Expected a sum of 30.00, got ", acc2.Sum())
+	}
+}
+
+func TestDeleteTransaction(t *testing.T) {
+	Init(true)
+	testacc := Account{1, "TestName", []Transaction{}}
+	CreateAccount(testacc.Name)
+	CreateTransaction(1, NewMoney(10.00))
+	
+	acc1, _ := GetAccount(1)
+	id := acc1.Transactions[0].Time
+	err := DeleteTransaction(1, id)
+	if err != nil {
+		t.Error("Expected no error, got ", err)
+	}
+	
+	acc2, _ := GetAccount(1)
+	if len(acc2.Transactions) != 0 {
+		t.Error("Expected no transactions, got ", len(acc2.Transactions))
+	}
+}
+
