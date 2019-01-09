a37995cf09f068652aec8372af97db78e474d8ed
Svip <svip@theinfosphere.org>
2019-01-09 21:35:18 +0100

    Added a web page to test the API.

--- src/main.go
+++ src/main.go
@@ -11,6 +11,7 @@ func main() {
 	db.Init(false)
 	http.HandleFunc("/account/", api.AccountEntry)
 	http.HandleFunc("/transaction/", api.TransactionEntry)
+	http.Handle("/", http.FileServer(http.Dir("media/")))
 	log.Println("Server listening on 8080.")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
